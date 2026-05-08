2849281a150de16996e4866a99ab308aafa94bb8
WarisDev <warishibrahim@aol.com>
2026-05-08 15:04:47 +0530

    single digit problem solved

---
+++ Day3/ifelse/Digits.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Digits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        if (number == 1) {
+            System.out.println("One");
+        } else if (number == 2) {
+            System.out.println("Two");
+        } else if (number == 3) {
+            System.out.println("Three");
+        } else if (number == 4) {
+            System.out.println("Four");
+        } else if (number == 5) {
+            System.out.println("Five");
+        } else if (number == 6) {
+            System.out.println("Six");
+        } else if (number == 7) {
+            System.out.println("Seven");
+        } else if (number == 8) {
+            System.out.println("Eight");
+        } else if (number == 9) {
+            System.out.println("Nine");
+        } else if (number == 0) {
+            System.out.println("Zero");
+        } else {
+            System.out.println("Invalid Number Enter Single Digit Number");
+        }
+
+        input.close();
+
+    }
+}
